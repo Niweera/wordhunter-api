1acb91a750ed274a209319ed1e93d27aa4031642
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-08-17 16:57:06 +0530

    add tests

---
+++ test/test-pages.js
@@ -0,0 +1,34 @@
+//Require the dev-dependencies
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = "https://wh.niweera.gq";
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe("WH-API/", () => {
+  beforeEach(done => setTimeout(done, 180000));
+  /*
+   * Test the /GET route
+   */
+
+  describe("/GET sad", () => {
+    it("It should GET all the words that can create with a,s,d", done => {
+      chai
+        .request(server)
+        .get("/sad")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a("array");
+          res.body[0].should.have.property("word");
+          res.body[0].word.should.be.a("string");
+          res.body[0].word.should.equal("ads");
+          res.body[0].should.have.property("definition");
+          res.body[0].definition.should.be.a("string");
+          res.body[0].definition.should.equal("An advertisement.");
+          done();
+        });
+    });
+  });
+});
